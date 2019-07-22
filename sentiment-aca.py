1dfdbf2644055e4630435d0bb64fd672dd209fc0
Ayten Huseynova <42516290+aytenhsnly@users.noreply.github.com>
2019-07-22 10:47:42 +1000

    TCC

--- sentiment-aca.py
+++ sentiment-aca.py
@@ -1,21 +1,10 @@
 import numpy as np
-import csv
 import pandas as pd
 import math
-import xlrd
-import matplotlib.pyplot as plt
-from matplotlib.ticker import FormatStrFormatter
 from glob import glob
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
-from collections import defaultdict
-from openpyxl import Workbook
-from pandas import ExcelFile
-from pandas import ExcelWriter
-from pandas import Series
-from collections import OrderedDict, defaultdict, Counter
-from itertools import chain
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
@@ -32,10 +21,10 @@ def analyse(comment, client, sid):
   magnitude = annotations.document_sentiment.magnitude
 
   # also run on NLTKs VADER
-
+  
   sentiment = sid.polarity_scores(comment)
-  print(score, magnitude, sentiment, comment)
-  return score, magnitude, sentiment['compound'], comment
+  print(score, magnitude, sentiment)
+  return score, magnitude, sentiment['compound']
 
 def process_files(files, columns_to_analyse, output):
   """process a list of files and save the output dataframe"""
@@ -53,75 +42,9 @@ def process_files(files, columns_to_analyse, output):
   # save the data as msgpack
   df.to_msgpack(f'{output}.msg')
 
-aca_post_files = ['survey-results/aca-post-workshop.results.2019-02-20.csv']
-#aca_pre_files = ['survey-results/aca-pre-workshop.results.2019-02-20.csv']
+aca_post_files = ['aca/aca-post-workshop.results.2019-01-29.csv']
+aca_pre_files = ['aca/aca-pre-workshop.results.2019-01-29.csv']
 columns = ['comments_day1_activities','comments_day1_info','comments_day2_programming_activities','comments_day2_workshop_lectures']
-# process_files(aca_post_files, columns, 'aca-post-data')
-# process_files(aca_pre_files, columns, 'aca-pre-data')
+process_files(aca_post_files, columns, 'aca-post-data')
+process_files(aca_pre_files, columns, 'aca-pre-data')
 
-df = pd.read_msgpack('aca-post-data.msg')
-
-columns_sent = ['question','comment','g-score','g-magnitude','nltk-score']
-
-# table1 = df.groupby('g-score')['comment'].\
-#         agg({'commentcount': pd.Series.nunique})
-
-# table1.plot.barh(align = 'center')
-
-plt.subplots_adjust(left=0.2)
-plt.margins(0.3)
-plt.ylabel('Sentiment Score')
-# plt.show()
-# plt.savefig('sentiment_comment.png')
-
-# df.groupby('g-score')['comment'].\
-    # agg({'commentcount': pd.Series.nunique,
-         # 'comment': lambda x: '|-|'.join(x)}, as_index = False, axis='columns')
-
-df = pd.read_csv('report1.csv')
-# f = lambda x: len(x["comment"].split("great")) -1
-# df["great"] = df.apply(f, axis=1)
-
-# print(df)
-# print(df.loc[df['g-score'] <= 0])
-# print(df.loc[df['g-score'] >= 0])
-# df = df.set_index(['g-score'])
-# print(df.loc[df.index.isin(['0.9'])])
-
-# file = open('report1.csv', encoding="utf8")
-file = open('challenge-evaluation-2019.results.2019-03-28.csv', encoding="utf8")
-reader = file.read()
-# Stopwords
-stopwords = set(line.strip() for line in open('stopwords.txt'))
-# stopwords = stopwords.union(set(['mr','mrs','one','two','said']))
-# Instantiate a dictionary, and for every word in the file,
-# Add to the dictionary if it doesn't exist. If it does, increase the count.
-wordcount = defaultdict(int)
-# To eliminate duplicates, remember to split by punctuation, and use case demiliters.
-for word in reader.lower().split():
-    word = word.replace(".","")
-    word = word.replace(",","")
-    word = word.replace(":","")
-    word = word.replace("\"","")
-    word = word.replace("!","")
-    word = word.replace("â€œ","")
-    word = word.replace("â€˜","")
-    word = word.replace("*","")
-    word = word.replace("|-|","")
-    if word not in stopwords:
-        wordcount[word] += 1
-print('finished replacing stuff')
-# Print most common word
-n_print = 50
-word_counter = Counter(wordcount)
-print('starting word count')
-for word, count in word_counter.most_common(n_print):
-    print(word, ": ", count)
-print('finished word count')
-# Close the file
-file.close()
-# Create a data frame of the most common words
-# Draw a bar chart
-lst = word_counter.most_common(n_print)
-df = pd.DataFrame(lst, columns = ['Word', 'Count'])
-df.plot.bar(x='Word',y='Count')
